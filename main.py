11c8ae4c5b082754d30f1ed3f32ba82b644fd565
Dean Gordon Chester <dean.g.chester@googlemail.com>
2018-01-12 16:51:05 +0000

    Scrapes LSE page.

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import tornado.ioloop
 import tornado.web
 import sys
-import requests
+from feedgen.feed import FeedGenerator
+from footsie import Share, Scrapper
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         # self.write("Hello, world")
-        page = requests.get('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/constituents-indices.html?index=UKX&industrySector=&page=1')
-        print(page.content)
+        # 
+        # print(page.content)
+        p1 = Scrapper.ScrapeWebSite('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/constituents-indices.html?index=UKX&industrySector=&page=1')
+        p2 = Scrapper.ScrapeWebSite('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/constituents-indices.html?index=UKX&industrySector=&page=2')    
+        p3 = Scrapper.ScrapeWebSite('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/constituents-indices.html?index=UKX&industrySector=&page=3')
+        p4 = Scrapper.ScrapeWebSite('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/constituents-indices.html?index=UKX&industrySector=&page=4')
+        p5 = Scrapper.ScrapeWebSite('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/constituents-indices.html?index=UKX&industrySector=&page=5')
+        p6 = Scrapper.ScrapeWebSite('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/constituents-indices.html?index=UKX&industrySector=&page=6')
         
+        shares= p1 + p2 + p3 + p4 + p5 + p6
+        print(len(shares))
+        fg = FeedGenerator()
+        fg.title("Footsie Shares")
+        fg.link(href='http://localhost:5000',  rel='alternate')
+        fg.description('FTSE prices from last 15 mintues.')
+        for s in shares:
+            fe = fg.add_entry()
+            s.get_rss_item(fe)
+        self.write(fg.rss_str())
+        # print(getattr(shares[0], "name"))
+        # self.write("hello")
 
 def make_app():
     return tornado.web.Application([
