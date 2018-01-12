fe649c4cf896896a51c755127df4095960fde854
Dean Gordon Chester <dean.g.chester@googlemail.com>
2018-01-12 12:09:09 +0000

    Added requests in to project.

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import tornado.ioloop
 import tornado.web
 import sys
+import request
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        # self.write("Hello, world")
+        page = requests.get('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/constituents-indices.html?index=UKX&industrySector=&page=1')
+        self.write(page)
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
+
 
 if __name__ == "__main__":
     app = make_app()
